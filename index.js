74f17643f1c18ab4a98f5f7d65ee9291eda42f92
pipe-justpipe <aremopipeloluwa@gmail.com>
2023-07-29 17:17:39 +0100

    i created a picture gallery

---
+++ index.js
@@ -0,0 +1,71 @@
+const change = document.getElementById("image1");
+change.addEventListener("click", (event)=>{
+    event.target.style.background = "rgb(149,85,62)";
+    event.target.style.color = "white"
+});
+
+const change2 = document.getElementById("image2");
+change2.addEventListener("click", (event)=>{
+    event.target.style.background = "rgb(233,142,164)";
+    event.target.style.color = "white"
+});
+
+const change3 = document.getElementById("image3");
+change3.addEventListener("click", (event)=>{
+    event.target.style.background = "rgb(57,93,186)";
+    event.target.style.color = "white"
+});
+
+const change4 = document.getElementById("image4");
+change4.addEventListener("click", (event)=>{
+    event.target.style.background = "rgb(245,133,30)";
+    event.target.style.color = "white"
+});
+
+const change5 = document.getElementById("image5");
+change5.addEventListener("click", (event)=>{
+    event.target.style.background = "rgb(252,211,53)";
+    event.target.style.color = "white"
+});
+
+const change6 = document.getElementById("image6");
+change6.addEventListener("click", (event)=>{
+    event.target.style.background = "rgb(121,142,156)";
+    event.target.style.color = "white"
+});
+
+const change7 = document.getElementById("image7");
+change7.addEventListener("click", (event)=>{
+    event.target.style.background = "rgb(196,143,66)";
+    event.target.style.color = "white"
+});
+
+const change8 = document.getElementById("image8");
+change8.addEventListener("click", (event)=>{
+    event.target.style.background = "rgb(153,204,57)";
+    event.target.style.color = "white"
+});
+
+const change9 = document.getElementById("image9");
+change9.addEventListener("click", (event)=>{
+    event.target.style.background = "rgb(3,4,6)";
+    event.target.style.color = "white"
+});
+
+const change10 = document.getElementById("image10");
+change10.addEventListener("click", (event)=>{
+    event.target.style.background = "rgb(228,125,237)";
+    event.target.style.color = "white"
+});
+
+const change11 = document.getElementById("image11");
+change11.addEventListener("click", (event)=>{
+    event.target.style.background = "rgb(227,28,75)";
+    event.target.style.color = "white"
+});
+
+const change12 = document.getElementById("image12");
+change12.addEventListener("click", (event)=>{
+    event.target.style.background = "rgb(4,7,9)";
+    event.target.style.color = "white"
+});
